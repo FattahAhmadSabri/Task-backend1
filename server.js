e439214a456e1e54276e76b74976d55ad31b7ba1
Fattah_Sabri <fassabri1@gmail.com>
2025-04-25 03:43:19 +0530

    updates_morgan for logg

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express= require("express");
 const cors = require("cors")
+const morgan = require("morgan")
 const helmet = require("helmet")
 const compression = require("compression")
 const productRoutes = require("./Routes/productRoutes")
@@ -12,7 +13,8 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(helmet())
-app.use(compression())
+app.use(compression());
+app.use(morgan("combined"))
 
 const port = process.env.PORT || 4000
 
